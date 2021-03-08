692b3c52f266ea27c79a351dd10b07d6748d2433
reza mehtari <remecom99@gmail.com>
2021-03-08 16:04:47 +0330

    create DriverController

---
+++ src/main/java/com/mohaymen/internship/carrental/control/DriverController.java
@@ -0,0 +1,29 @@
+package com.mohaymen.internship.carrental.control;
+
+import com.mohaymen.internship.carrental.model.Driver;
+import com.mohaymen.internship.carrental.service.DriverService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@AllArgsConstructor
+@RestController
+public class DriverController {
+    DriverService service;
+
+    @PostMapping("/driver")
+    public ResponseEntity<Integer> createDriver(@RequestBody Driver newDriver) {
+        return new ResponseEntity<>(service.create(newDriver), HttpStatus.OK);
+    }
+
+    @GetMapping("/driver/{id}")
+    public ResponseEntity<?> getDriver(@PathVariable int id) throws Exception {
+        Optional<Driver> loaded = service.get(id);
+        if (loaded.isPresent())
+            return new ResponseEntity<>(loaded.get(), HttpStatus.OK);
+        return new ResponseEntity<>("there is no driver with this id", HttpStatus.NOT_FOUND);
+    }
+}
