10af41aa024d3d85e333fc1357a52acea92b58de
reza mehtari <remecom99@gmail.com>
2021-03-16 12:37:15 +0330

    remove throwing Exc from get

--- src/main/java/com/mohaymen/internship/carrental/service/DriverService.java
+++ src/main/java/com/mohaymen/internship/carrental/service/DriverService.java
@@ -8,5 +8,5 @@ import java.util.Optional;
 @Service
 public interface DriverService {
     int create(Driver driver);
-    Optional<Driver> get(int id) throws Exception;
+    Optional<Driver> get(int id);
 }
